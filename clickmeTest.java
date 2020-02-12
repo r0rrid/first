50433b64ab928405b4fb94587dee10751a7ddbfc
Randy <randyorr44@gmail.com>
2020-02-11 18:35:31 -0700

    added comments

--- clickmeTest.java
+++ clickmeTest.java
@@ -1,3 +1,5 @@
+// This script will use selenium to initiate a chrome browser, execute a click on a button
+// and test for the existence of text on the page
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +11,7 @@ public static void main(String[] args) {
 
 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 
+// Define command options to be used when the browser is instantiated
 ChromeOptions options = new ChromeOptions();
 options.addArguments("--headless");
 options.addArguments("--no-sandbox");
@@ -16,13 +19,13 @@ options.addArguments("--disable-dev-shm-usage");
 
 WebDriver driver = new ChromeDriver(options);
 
-//String baseUrl = "http://192.168.0.3";
-
+// Define URL to be referenced
 driver.get("http://localhost");
-//System.out.println(driver.getPageSource());
+
+// Click on the button
 driver.findElement(By.id("clickme")).click();
 
-System.out.println(driver.getPageSource());
+// Verify that the page now has desired text on the page - fail the test if it isn't found
 if (driver.getPageSource().contains("Clicked")){;
 
 System.out.println("Pass");
